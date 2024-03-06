d49846e852bd8d4d261cdfd96f9dd4b6e9e050ae
Noz Blue <n1v0x.5127e@gmail.com>
2024-03-07 01:14:56 +0900

    プロジェクト ファイルを追加します。

---
+++ DisplayMotionCreatorCore/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DisplayMotionCreatorCore
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
