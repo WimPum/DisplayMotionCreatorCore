7ee9d22cf345c1cac4efac78f1d154ba90036914
Noz Blue <n1v0x.5127e@gmail.com>
2024-03-07 01:22:55 +0900

    現在の姿 with BinaryWriter これからはList生成

---
+++ DisplayMotionCreatorCore/CSV2VMD.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Enumeration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32;
+using Microsoft.VisualBasic;
+using System.Windows;
+
+namespace DisplayMotionCreatorCore
+{
+    /// ここにwrite_vmd_file
+    internal class CSV2VMD
+    {
+        //public void write_vmd_file(List<List<String>> keyframes) {
+
+        public void write_vmd_file(byte[] keyframes) {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileName = "output.vmd";
+            sfd.Filter = "VMDファイル (*.vmd)|*.vmd";
+            sfd.Title = "名前を付けて保存";
+            if (sfd.ShowDialog() == true){
+                //File.WriteAllBytes(sfd.FileName, keyframes)
+                save_binary(sfd.FileName, keyframes);
+                MessageBox.Show("XXXbpm、X拍子、XXX秒で生成しました。キー数XXXXです。\n" +
+                    "MMDのキー登録数の上限は20000ですので注意してください。", "出力完了", 
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
+            //using (BinaryWriter writer = new BinaryWriter(File.OpenWrite()));
+        }
+
+        public void save_binary(string filename, byte[] keyframes) {
+            Encoding encoding = new UTF8Encoding();
+            using (BinaryWriter writer = new BinaryWriter(File.OpenWrite(filename))){
+                writer.Write(encoding.GetBytes("Vocaloid Motion Data 0002\x00\x00\x00\x00\x00"));
+                writer.Write(encoding.GetBytes("Electone3DModel     "));
+            }
+        }
+    }
+}
--- DisplayMotionCreatorCore/MainWindow.xaml.cs
+++ DisplayMotionCreatorCore/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,17 +9,33 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace DisplayMotionCreatorCore
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// こっちにWindowコントロールをいれる（つまりほとんど関数の呼び出しのみ）
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void confirm_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.Write("GenSTART\n");
+        }
+
+        private void debug_Click(object sender, RoutedEventArgs e)
+        {
+            //List<string> list = new List<string>();
+            Debug.Write("Debugging/Opening\n");
+            byte[] data = File.ReadAllBytes("PATHTO/output.vmd"); //すぐ消すからね!
+            CSV2VMD cSV2VMD = new CSV2VMD();
+            cSV2VMD.write_vmd_file(data);
         }
     }
 }
